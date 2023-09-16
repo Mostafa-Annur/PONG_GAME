2550df2dcae3259850c22692cf574eb3c75f4579
Mostafa-Annur <88188102+Mostafa-Annur@users.noreply.github.com>
2023-09-16 23:11:49 +0600

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,17 @@
+package pong;
+import java.awt.Graphics;
+public class Ball {
+	
+	public int x,y,width ,height;
+	
+public Ball() {
+	
+}
+public void update(Paddle paddle1,Paddle paddle2) {
+	
+}
+public void render (Graphics g) 
+{
+
+}
+}
---
+++ Pong.java
@@ -0,0 +1,139 @@
+package pong;
+import java.awt.BasicStroke;
+import java.awt.Color;
+//import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+
+public  class Pong implements ActionListener,KeyListener
+{
+    public static Pong pong;
+    
+    public int width=1200,height=800;
+    
+    public Renderer renderer;
+    
+    public Paddle player1;
+    public Paddle player2;
+    public boolean bot=false;
+    public boolean w,s,up,down;
+    
+  public Pong()
+  {  
+	  Timer timer=new Timer(20,this);
+	  JFrame jframe=new JFrame("Pong");
+	  renderer = new Renderer();
+	  jframe.setSize(width+20, height);
+	  jframe.setVisible(true);
+	  jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  jframe.add(renderer);
+	  jframe.addKeyListener(this);
+	  
+	  start();
+	  timer.start();
+	  
+  }  
+	  
+	  public void start() {
+		  player1=new Paddle(this,1);
+		  player2=new Paddle(this,2);
+	  }
+    
+  public void update() 
+  {
+	 if (w) 
+	 {
+		 player1.move(true);
+	 }
+	 if (s) 
+	 {
+		 player1.move(false);
+	 }
+	 if (up) 
+	 {
+		 player2.move(true);
+	 }
+	 if (down) 
+	 {
+		 player2.move(false);
+	 }
+	 
+  }
+  public void render(Graphics2D g)
+  {
+	  g.setColor(Color.blue);
+	  
+	  g.fillRect(0, 0, width, height);
+	  
+      g.setColor(Color.white);
+      
+      g.setStroke(new BasicStroke(5f));
+      
+      g.drawLine(width/2,0,width/2 , height);
+      
+      
+	player1.render(g);
+	player2.render(g);
+  }
+  public void actionPerformed(ActionEvent e) 
+  {
+	update();
+	renderer.repaint();
+  }
+  public static void main(String[] args)
+  {
+    pong=new Pong();  
+  }
+
+@Override
+public void keyPressed(KeyEvent e) 
+{
+	int id = e.getKeyCode();
+	if(id==KeyEvent.VK_W)
+	{
+		w=true;
+	}
+	if(id==KeyEvent.VK_S) {
+		s=true;
+	}
+	if(id==KeyEvent.VK_UP) {
+		up=true;
+	}
+	if(id==KeyEvent.VK_DOWN) {
+		down=true;
+	}
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	int id = e.getKeyCode();
+	if(id==KeyEvent.VK_W)
+	{
+		w=false;
+	}
+	if(id==KeyEvent.VK_S) {
+		s=false;
+	}
+	if(id==KeyEvent.VK_UP) {
+		up=false;
+	}
+	if(id==KeyEvent.VK_DOWN) {
+		down=false;
+	}
+	
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	
+	
+}
+ }
+
